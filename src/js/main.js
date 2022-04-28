edf7b768cba8408aeb05116fc7a3cac30bc338a6
Marcin <m.ziola960111@gmail.com>
2022-04-28 12:07:44 +0200

    start js

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+const reviewItems = document.querySelectorAll('card__review-item')
+const buttonSubmit = document.querySelector('card__submit')
+const span = document.querySelector('choice')
+
+reviewItems.forEach(e => {
+	console.log(e.target)
+})
