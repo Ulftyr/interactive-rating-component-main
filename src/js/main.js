cb137d33d8a0c6c668e62f112994ec0772cfaed0
Marcin <m.ziola960111@gmail.com>
2022-04-29 14:10:11 +0200

    finish project

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,26 @@
-const reviewItems = document.querySelectorAll('card__review-item')
-const buttonSubmit = document.querySelector('card__submit')
-const span = document.querySelector('choice')
+const reviewItems = document.querySelectorAll('.card__review-item')
+const stars = document.querySelector('.card__review')
+const submitBtn = document.querySelector('.card__submit')
+const span = document.querySelector('.choice')
+const reverse = document.querySelector('.reverse-card')
+const front = document.querySelector('.front-card')
 
-reviewItems.forEach(e => {
-	console.log(e.target)
+reviewItems.forEach(el => {
+	const programStart = () => {
+		let rating = el.textContent
+		reviewItems.forEach(el => {
+			el.classList.contains('clicked')
+			el.classList.remove('clicked')
+		})
+		el.classList.add('clicked')
+		submitBtn.addEventListener('click', () => {
+			front.classList.add('not')
+			front.classList.remove('card')
+			reverse.classList.remove('not')
+			reverse.classList.add('card')
+			span.textContent = rating
+		})
+	}
+
+	el.addEventListener('click', programStart)
 })
